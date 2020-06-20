2ca2dbe0bf4badb1ba85a61d73539a7dec83e97f
Mohammad Afandy <m.afandy@docotel.com>
2020-06-20 16:56:26 +0700

    add postinstall and base backend url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://127.0.0.1:8000',
+        target: process.env.VUE_APP_BASE_URL || 'http://127.0.0.1:8000',
         pathRewrite: {'^/api' : ''}
       }
     }
