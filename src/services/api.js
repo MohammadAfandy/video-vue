d23b83996fa312c7f6f3159ea4414f06e8ed4241
Mohammad Afandy <m.afandy@docotel.com>
2020-06-20 18:03:28 +0700

    change baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: "/api",
+    baseURL: process.env.VUE_APP_BASEURL || 'http://127.0.0.1:8000',
     withCredentials: false,
     headers: {
       Accept: "application/json",
