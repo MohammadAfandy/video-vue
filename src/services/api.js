62a612832c36ab7fab215be5af8efd720c231ce4
Mohammad Afandy <m.afandy@docotel.com>
2020-06-20 18:38:10 +0700

    fix server.js because vue history mode and change .env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 export default () => {
-  console.log(process.env.VUE_APP_BASEURL)
   return axios.create({
-    baseURL: process.env.VUE_APP_BASEURL || 'http://127.0.0.1:8000',
+    baseURL: process.env.VUE_APP_BACKEND_URL || 'http://127.0.0.1:8000',
     withCredentials: false,
     headers: {
       Accept: "application/json",
