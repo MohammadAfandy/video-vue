07cf639285d92e9cc4fe61d7624a7b070c6cf6e8
Mohammad Afandy <m.afandy@docotel.com>
2020-06-20 18:18:40 +0700

    debug .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export default () => {
+  console.log(process.env.VUE_APP_BASEURL)
   return axios.create({
     baseURL: process.env.VUE_APP_BASEURL || 'http://127.0.0.1:8000',
     withCredentials: false,
